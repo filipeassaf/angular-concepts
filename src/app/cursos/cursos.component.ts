076e2a5b9955632754f38f879b41df6d1375dd27
Filipe Assaf <filipevalles@gmail.com>
2021-03-25 21:50:06 -0300

    Loiane Training

---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+
+  cursos: string[] = ['Angular','React','Node'];
+
+  constructor() {
+
+    this.nomePortal = 'loiane.training';
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
